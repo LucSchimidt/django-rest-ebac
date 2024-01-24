68a77a84d6b57b36f139c52494df656c82f060c6
LucSchimidt <llourencoschimidt@gmail.com>
2024-01-24 09:50:47 -0300

    autenticacao de Token criada para orders.

--- ebac/urls.py
+++ ebac/urls.py
@@ -2,6 +2,7 @@ import debug_toolbar
 
 from django.contrib import admin
 from django.urls import path, re_path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
     re_path('bookstore/(?P<version>(v1|v2))/', include('products.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ]
--- products/viewsets/product_viewset.py
+++ products/viewsets/product_viewset.py
@@ -1,10 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from products.models import Product
 from products.serializers import ProductSerializer
 
 
+
 class ProductViewSet(ModelViewSet):
+    authentication_classes = [BasicAuthentication, SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
